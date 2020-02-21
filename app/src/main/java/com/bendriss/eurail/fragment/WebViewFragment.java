eae3d0c682d1643416a48c9892851a2bbbd79acd
Tarek <tarek.bendriss@natan.fr>
2020-02-21 16:44:11 +0100

    Calculate divs

--- app/src/main/java/com/bendriss/eurail/fragment/WebViewFragment.java
+++ app/src/main/java/com/bendriss/eurail/fragment/WebViewFragment.java
@@ -20,6 +20,14 @@ import android.widget.Toast;
 
 import com.bendriss.eurail.R;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 public class WebViewFragment extends Fragment {
     private WebView eurailWebView;
@@ -81,6 +89,9 @@ public class WebViewFragment extends Fragment {
         final boolean[] loadingFinished = {true};
         final boolean[] redirect = {false};
 
+        String s = "\u003Chtml>\u003C/html>\u003Chtml>\u003C/html>";
+        s= s.replace("\u003C","<");
+        Log.e("HTML","hedha 11  "+s);
         eurailWebView.setWebViewClient(new WebViewClient() {
 
             @Override
@@ -135,15 +146,78 @@ public class WebViewFragment extends Fragment {
     private void test(WebView eurailWebView)
     {
         eurailWebView.evaluateJavascript(
-                "(function() { return document.getElementsByTagName('div').length; })();",
+                "(function() { return ('<html>'+document.getElementsByTagName('body')[0].innerHTML+'</html>'); })();",
                 //"(function() { return document.querySelectorAll('div').length; })();",
+                // \u003C
                 new ValueCallback<String>() {
                     @Override
                     public void onReceiveValue(String html) {
-                        Log.d("HTML", html);
+                        String s = html;
+                        //s = s.replace("\u003C","<");
+
+                        s= s.replace("\\u003C","<");
+                        s= s.replace("\\u003C","<");
+                        //Log.e("HTML","hedha   "+s);
+
+                        //html = html.replace("\u003C/","/");
+
+                        //Log.d("HTML",html);
+                        Log.e("HTML", "1#"+countOccurences2(s,"\\u003C/div")+"");
+                        Log.e("HTML", "2#"+countOccurences(html,"\\u003Cdiv")+"");
+                    }
+                });
+        eurailWebView.evaluateJavascript(
+                //"(function() { return ('<html>'+document.getElementsByTagName('body')[0].innerHTML+'</html>'); })();",
+                "(function() { return document.querySelectorAll('div').length; })();",
+                // \u003C
+                new ValueCallback<String>() {
+                    @Override
+                    public void onReceiveValue(String html) {
+                        Log.e("HTML","3#"+html);
                     }
                 });
     }
 
+    private int countOccurences(String str, String word)
+    {
+        // split the string by spaces in a
+        String a[] = str.split(" ");
+        // search for pattern in a
+        int count = 0;
+        for (int i = 0; i < a.length; i++)
+        {
+            // if match found increase count
+            if (word.equals(a[i]))
+            {
+                count=count+1;
+                //appendLog(word);
+                //writeToFile(a[i],getContext());
+                //Log.e("HTML",word);
+                //Log.e("HTML",a[i]);
+            }
+        }
+
+        return count;
+    }
+
+    private int countOccurences2(String str,String word)
+    {
+        // split the string by spaces in a
+        // search for pattern in a
+        int count = 0;
+        //Log.e("HTML","xxxx");
+        System.out.println("xxxxxx "+str);
+        //Log.wtf("xxxxxx ",str);
+        for (int i = 0; i < str.length(); i++)
+        {
+            // if match found increase count
+            if(str.charAt(i)=='<' && str.charAt(i+1)=='d' && str.charAt(i+2)=='i' && str.charAt(i+3)=='v')
+            {count++;
+            //Log.e("HTML",str.charAt(i)+str.charAt(i+1)+str.charAt(i+2)+str.charAt(+3)+"div");
+            }
+        }
+
+        return count;
+    }
 
 }
